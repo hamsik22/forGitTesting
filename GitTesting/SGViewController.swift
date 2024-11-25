b55a4e65d236e8bd610088125d57c27dd3a7303b
Jeon_Seong_gyu <junsung3807@naver.com>
2024-11-25 11:34:56 +0900

    Feat: create sg congroller

---
+++ GitTesting/SGViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SGViewController.swift
+//  GitTesting
+//
+//  Created by 전성규 on 11/25/24.
+//
+
+import UIKit
+
+final class SGViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 전성규
+    }
+}

e647a00b317198097079a6fc265e97fbb6e1837e
HamSik <payback9488@gmail.com>
2024-11-25 11:24:48 +0900

    Initial Commit

---
+++ GitTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTesting
+//
+//  Created by 황석현 on 11/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitTestingTests/GitTestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestingTests.swift
+//  GitTestingTests
+//
+//  Created by 황석현 on 11/25/24.
+//
+
+import Testing
+@testable import GitTesting
+
+struct GitTestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

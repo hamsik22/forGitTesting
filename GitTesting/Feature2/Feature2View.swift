5f2f5668bb2753da787efca7dff38545f2ebd98f
Hamsik22 <payback9488@gmail.com>
2025-11-13 23:19:30 +0900

    [Add]: Feature2View 추가

---
+++ GitTesting/Feature2/Feature2View.swift
@@ -0,0 +1,18 @@
+//
+//  Feature2View.swift
+//  GitTesting
+//
+//  Created by 황석현 on 11/13/25.
+//
+
+import SwiftUI
+
+struct Feature2View: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    Feature2View()
+}
